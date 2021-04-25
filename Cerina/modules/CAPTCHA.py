91f7762b6a77a51413df5d3c89ac475bff186d77
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-25 22:09:45 +0530

    Update CAPTCHA.py

--- Cerina/modules/CAPTCHA.py
+++ Cerina/modules/CAPTCHA.py
@@ -1,4 +1,4 @@
-import Cerina.modules.sql.captcha_sql as sql
+from Cerina.modules.sql.captcha_sql import *
 from Cerina import tbot, CMD_HELP
 from Cerina.events import is_admin
 from . import can_change_info, ck_admin
@@ -47,7 +47,7 @@ async def lel(event):
    return await event.reply("Only admins can execute this command.")
  if not await can_change_info(message=event):
    return await event.reply("You are missing the following rights to use this command:CanChangeInfo!")
- settings = sql.get_mode(event.chat_id)
+ settings = get_mode(event.chat_id)
  if not args:
   if settings == True:
    await event.reply("Users will be asked to complete a CAPTCHA before being allowed to speak in the chat.\nTo change this setting, try this command again followed by one of yes/no/on/off")
@@ -56,13 +56,13 @@ async def lel(event):
  elif args in turnon:
   mode = True
   await event.reply("CAPTCHAs have been enabled. I will now mute people when they join.")
-  x = sql.set_mode(event.chat_id, mode)
+  x = set_mode(event.chat_id, mode)
   if not x:
-    sql.set_captcha(event.chat_id, "button")
+    set_captcha(event.chat_id, "button")
  elif args in turnoff:
   mode = False
   await event.reply("CAPTCHAs have been disabled. Users can join normally.")
-  x = sql.set_mode(event.chat_id, mode)
+  x = set_mode(event.chat_id, mode)
 
 @tbot.on(events.NewMessage(pattern="^[!?/]captchatime ?(.*)"))
 @is_admin
@@ -70,7 +70,7 @@ async def lel(event, perm):
  if not perm.change_info:
          await event.reply("You are missing the following rights to use this command:CanChangeInfo!")
          return
- settings = sql.get_unmute_time(event.chat_id)
+ settings = get_unmute_time(event.chat_id)
  args = event.pattern_match.group(1)
  if not args:
   if settings == 0:
@@ -85,12 +85,12 @@ async def lel(event, perm):
     await event.reply(f"If users haven't unmuted themselves after {tt}, they will be unmuted automatically.\nTo change the CAPTCHA mute time, try this command again with a time value.\nExample time values: 4m = 4 minutes, 3h = 3 hours, 6d = 6 days, 5w = 5 weeks.")
  elif args in turnoff:
   mutetime = 0
-  x = sql.set_unmute_time(event.chat_id, mutetime)
+  x = set_unmute_time(event.chat_id, mutetime)
   await event.reply("I will now mute users for an indeterminate amount of time. The only way for them to get unmuted will be to complete the CAPTCHA.")
  elif args:
   mutetime = await extract_time(event, args)
   if mutetime:
-   x = sql.set_unmute_time(event.chat_id, mutetime)
+   x = set_unmute_time(event.chat_id, mutetime)
    await event.reply(f"I will now mute people for {args} when they join - or until they solve the CAPTCHA in the welcome message.")
   
 @tbot.on(events.NewMessage(pattern="^[!?/]captchamode ?(.*)"))
@@ -100,7 +100,7 @@ async def lel(event, perm):
  if not perm.change_info:
          await event.reply("You are missing the following rights to use this command:CanChangeInfo!")
          return
- settings = sql.get_style(event.chat_id)
+ settings = get_style(event.chat_id)
  args = event.pattern_match.group(1)
  if not args:
   text = ""
@@ -140,9 +140,9 @@ Available CAPTCHA modes are: button/math/text/multibutton
   style = args
   text = "CAPTCHAs set to **{}**.".format(style)
   await event.reply(text)
-  x = sql.set_style(event.chat_id, style)
+  x = set_style(event.chat_id, style)
   if not x:
-   sql.set_captcha(event.chat_id, style)
+   set_captcha(event.chat_id, style)
  else:
   await event.reply(f"{args} is not a recognised CAPTCHA mode! Try one of: button/math/text/multibutton")
 
@@ -163,7 +163,7 @@ async def lel(event):
   if args in avoid:
    return
  time = 300
- settings = sql.get_time(event.chat_id)
+ settings = get_time(event.chat_id)
  if not args:
    if settings == False or settings == 0:
     await event.reply("""Users that don't complete their CAPTCHA are allowed to stay in the chat, muted, and can complete the CAPTCHA whenever.
@@ -186,7 +186,7 @@ To change this setting, try this command again followed by one of yes/no/on/off"
  elif args in optionsn:
   time = 0
   await event.reply("I will no longer kick people that haven't solved the CAPTCHA.")
- x = sql.set_time(event.chat_id, time)
+ x = set_time(event.chat_id, time)
 
 @tbot.on(events.NewMessage(pattern="^[!?/]captchakicktime ?(.*)"))
 @is_admin
@@ -196,7 +196,7 @@ async def lel(event, perm):
          return
  time = event.pattern_match.group(1)
  if time: mutetime = await extract_time(event, time)
- settings = sql.get_time(event.chat_id)
+ settings = get_time(event.chat_id)
  if not time:
    if settings == False or settings == 0:
     await event.reply("Users that don't complete their CAPTCHA are allowed to stay in the chat, muted, and can complete the CAPTCHA whenever.To change this setting, try this command again followed by one of yes/no/on/off")
@@ -211,7 +211,7 @@ async def lel(event, perm):
  if mutetime:
   if mutetime >= 86400 or mutetime < 300:
     return await event.reply("The welcome kick time can only be between 5 minutes, and 1 day. Please choose another time.")
-  x = sql.set_time(event.chat_id, mutetime)
+  x = set_time(event.chat_id, mutetime)
   if not x:
    return await event.reply("Enable captcha first!.")
   await event.reply(f"Welcome kick time has been set to {time}.")
