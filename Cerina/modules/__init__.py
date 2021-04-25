efb7ce3dd467e43ae53cf28d9ea8a859f2aa69bf
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-25 22:21:54 +0530

    Update __init__.py

--- Cerina/modules/__init__.py
+++ Cerina/modules/__init__.py
@@ -1,6 +1,7 @@
 from Cerina import tbot, MONGO_DB_URI
 from telethon import functions, types
 from pymongo import MongoClient
+import random
 import Cerina.modules.sql.elevated_sql as sql
 
 #Setup SUDO
@@ -73,3 +74,20 @@ async def ck_admin(event, user):
     except:
         is_mod = False
     return is_mod
+
+number_list = ['0','1','2','3','4','5','6','7','8','9']
+alphabet_lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+alphabet_uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+def gen_captcha(captcha_string_size=10):
+    captcha_string_list = []
+    base_char = alphabet_lowercase + alphabet_uppercase + number_list
+    for i in range(captcha_string_size):
+        char = random.choice(base_char)
+        captcha_string_list.append(char)
+    captcha_string = '' 
+    for item in captcha_string_list:
+        captcha_string += str(item)
+    return captcha_string
+
+
