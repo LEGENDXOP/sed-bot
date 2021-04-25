10ea87ae4449570303e36add6fa2b00a8613b9ec
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-25 22:41:47 +0530

    Update __init__.py

--- Cerina/modules/__init__.py
+++ Cerina/modules/__init__.py
@@ -92,7 +92,7 @@ def gen_captcha(captcha_string_size=10):
     return captcha_string
 
 def gen_img_captcha(captcha_string_size=10):
- image_captcha = ImageCaptcha(width = 800, height = 540, font_sizes=[74, 90, 100])
+ image_captcha = ImageCaptcha(width = 1440, height = 900, font_sizes=[174, 190, 1100])
  text = gen_captcha(captcha_string_size)
  image_file = "./"+ "captcha.png"
  image_captcha.write(text, image_file)
